488273964f74a7be520d3b93e3df74d76038679e
Dorian Marié <dorian@dorianmarie.com>
2024-04-04 01:54:19 +0200

    linting

--- spec/anonymize_json_spec.rb
+++ spec/anonymize_json_spec.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
 RSpec.describe "anonymize-json" do
   it "works" do
-    expect(`bin/anonymize-json '{"a":"Secret123"}'`).to eq(%({\n  "a": "Aaaaaa000"\n}\n))
+    expect(`bin/anonymize-json '{"a":"Secret123"}'`).to eq(
+      %({\n  "a": "Aaaaaa000"\n}\n)
+    )
   end
 end
